56cb7ba96d75cfa0ca7358cf924c7802bbf57332
VORTEX <nsreekumar6@gmail.com>
2023-11-22 10:56:05 +0530

    bleh

--- dualram.v
+++ dualram.v
@@ -1,30 +1,30 @@
 module raminfr
-    (clk1,clk2, wea, addra, addrb, dia,dib,web, doa, dob);
+(clk1,clk2, wea, addra, addrb, dia,dib,web, doa, dob);
 
-  input clk1;
- input clk2;
-  input wea;
-  input web;
-  input [4:0] addra;
-  input [4:0] addrb;
-  input [3:0] dia;
-  input [3:0] dib;
-  output [3:0] doa;
-  output [3:0] dob;
-  reg [3:0] ram [31:0];
-  reg [4:0] read_addra;
-  reg [4:0] read_addrb;
- always@(posedge clk1) begin
-   if (wea) begin
-     ram[addra] <= dia;
- end
-read_addra <= addra;
+input clk1;
+input clk2;
+input wea;
+input web;
+input [3:0] addra;
+input [3:0] addrb;
+input [3:0] dia;
+input [3:0] dib;
+output [3:0] doa;
+output [3:0] dob;
+reg [3:0] ram [31:0];
+reg [3:0] read_addra;
+reg [3:0] read_addrb;
+always@(posedge clk1) begin
+    if (wea) begin
+        ram[addra] <= dia;
+    end
+    read_addra <= addra;
 end
 always @(posedge clk2) begin
-  if (web) begin
-    ram[addrb] <= dib;
- end
-read_addrb <= addrb;
+    if (web) begin
+        ram[addrb] <= dib;
+    end
+    read_addrb <= addrb;
 end
 assign dob = ram[read_addrb];
 assign doa = ram[read_addra];
--- test.v
+++ test.v
@@ -1,61 +1,89 @@
 module pff;
 
+reg clk1;
+reg clk2;
+reg wea;
+reg [3:0] addra;
+reg [3:0] addrb;
+reg [3:0] dia;
+reg [3:0] dib;
+reg web;
+wire [3:0] doa;
+wire [3:0] dob;
+raminfr uut (
+    .clk1(clk1),
+    .clk2(clk2),
+    .wea(wea),
+    .addra(addra),
+    .addrb(addrb),
+    .dia(dia),
+    .dib(dib),
+    .web(web),
+    .doa(doa),
+    .dob(dob)
+);
 
-	reg clk1;
-	reg clk2;
-	reg wea;
-	reg [4:0] addra;
-	reg [4:0] addrb;
-	reg [3:0] dia;
-	reg [3:0] dib;
-	reg web;
-	wire [3:0] doa;
-	wire [3:0] dob;
-	raminfr uut (
-		.clk1(clk1),
-		.clk2(clk2),
-		.wea(wea),
-		.addra(addra),
-		.addrb(addrb),
-		.dia(dia),
-		.dib(dib),
-		.web(web),
-		.doa(doa),
-		.dob(dob)
-	);
+initial begin
 
-	initial begin
+    clk1 = 0;
+    clk2 = 0;
+    wea = 0;
+    web = 0;
+    addra = 0;
+    addrb = 0;
+    dia = 0;
+    dib = 0;
 
-		clk1 = 0;
-		clk2 = 0;
-		wea = 0;
-		addra = 0;
-		addrb = 0;
-		dia = 0;
-		dib = 0;
-		web = 0;
-
-		forever begin
-		#5 clk1=~clk1;
-		#5 clk2=~clk2;
-
-		end
+    forever begin
+        #3 clk1=~clk1;
+        #1 clk2=~clk2;
     end
-		initial begin
-          wea=1;
-          web=1;
-		dia=4'ha;
-		addra=5'd6;
-		addrb=5'd7;
-	 dib=4'hb;
+end
 
-		#100 $finish;
-          end
+initial begin
+    wea=1;
+    web=1;
+    addrb=4'd7;
+    addra=4'd6;
+    dia=4'ha;
+    dib=4'hb;
+    #5
+    dia=4'hc;
+    dib=4'hd;
 
-        initial begin
+    #10
+    wea=0;
+    web=0;
+    addra=4'd6;
+    addrb=4'd7;
+    dia=4'ha;
+    dib=4'hb;
+    #5
+    dia=4'hc;
+    dib=4'hd;
+
+    #10
+    wea=1;
+    web=0;
+    addra=4'd2;
+    addrb=4'd9;
+    dia=4'h0;
+    dib=4'h1;
+
+    #10
+    wea=0;
+    web=1;
+    addra=4'd2;
+    addrb=4'd9;
+    dia=4'h0;
+    dib=4'h1;
+    #10 $finish;
+end
+
+initial begin
     $dumpfile("bleh.vcd");
     $dumpvars(0,pff);
-  end
+end
 
 
 endmodule
